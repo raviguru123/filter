2d75dc80a226851b065c7617124db6150b6b09b3
raviguru123 <raviguruiitr@gmail.com>
2016-05-25 15:23:31 +0530

    after filter implementation

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
 var app=angular.module('MyModule', []);
 
 app.controller('MyController',function($scope){
-	$scope.name="Ravi kumar guru";
-	$scope.clickFunction=function(){
-		alert("hello");
-	}
+	$scope.friends=[{name:"ravi"},{name:"anand"},{name:"manoj"},{name:"rajdeep"},{name:"gautam"}];
+$scope.letter="g";
 });
 
+app.filter("nameStartWithA",nameStartWithA);
 
+function nameStartWithA(){
+
+	return function(items,letter){
+		var array=[];
+		var newletterMatch=new RegExp(letter,i);
+		for(var i=0;i<items.length;i++)
+		{
+			
+			if(newletterMatch.test(items[i].name.substring(0,1)))
+			{
+				array.push(items[i]);
+			}
+		}
+		return array;
+	}
+}
+
+app.filter("makeUpperCase",makeUpperCase);
+function makeUpperCase(){
+	return function(item){
+		return item.toUpperCase();
+	}
+}
