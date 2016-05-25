e33fa433397e6a937275b66fdc6f3a9b772ea893
raviguru123 <raviguruiitr@gmail.com>
2016-05-25 19:18:54 +0530

    angular function call from outside

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var globalVariable="ravi kumar guru global";
 var app=angular.module('MyModule', []);
 
 app.controller('MyController', ['$scope', function($scope){
@@ -23,24 +24,25 @@ function nameStartWithA(){
 	}
 }
 
-app.controller('initlization', ['$scope', function($scope){
-	
+app.controller('initlization', ['$scope','$window', function($scope,$window){
+	//function call when complete view loaded in DOM
 	$scope.$on('$viewContentLoaded', function(){
-		alert("Dom Content Loaded");
+		alert($window.globalVariable);
 	});
+	//fucntion call when DOM content is ready
 	angular.element(document).ready(function(){
-		alert("document ready function in angularjs")
+		alert($window.globalVariable);
 	});
-
+	//init test from DOM using ng-init directory
 	$scope.initFunction=function(){
-			$scope.secondFunction();
-			$scope.thirdFunction();
+		$scope.secondFunction();
+
 	};
 	$scope.secondFunction=function(){
 		//alert("first1");
 	}
-	$scope.thirdFunction=function(){
-		//alert("second");
+	$scope.callFromOutside=function(){
+		alert("successfull call from outside angular");
 	}
 }])
 app.filter("makeUpperCase",makeUpperCase);
@@ -50,4 +52,16 @@ function makeUpperCase(){
 	}
 }
 
+
+
+document.addEventListener("DOMContentLoaded",function(event){
+	console.log("dom content loaded");
+	document.getElementById("btncall").addEventListener("click", function(){
+		var scopeRef=angular.element(document.getElementsByName("innerController")).scope();
+		scopeRef.$apply(function(){
+			scopeRef.callFromOutside();
+		})	
+	});
+
+})
 
